0f967806461a7e4ada92246d90fe98467d9bb989
Sanskruti-Shete <shetesanskruti066@gmail.com>
2025-05-18 11:35:11 +0530

    cursor

--- src/main.js
+++ src/main.js
@@ -10,3 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
     loop: true,
   });
 });
+const cursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.transform = `translate(${e.clientX - 10}px, ${e.clientY - 10}px)`;
+});
